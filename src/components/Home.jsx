27725c60090b73fcf1cb0b05a4aae21e4eb0f4f1
andresrh11 <andresr10020@gmail.com>
2022-11-18 11:10:53 -0500

    Home

---
+++ src/components/Home.jsx
@@ -0,0 +1,17 @@
+import Header from "./Header";
+import Slider from "./Slider";
+import Navbar from "./Navbar";
+import Contact from "./Contact";
+import Footer from "./Footer";
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <Navbar />
+      <Slider />
+      <Contact />
+      <Footer />
+    </>
+  );
+}
