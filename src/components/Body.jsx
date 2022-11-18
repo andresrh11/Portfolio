19d82ec8ddf5c6dd3ccdf2c5a69a21a16647e2da
andresrh11 <andresr10020@gmail.com>
2022-11-18 18:12:55 -0500

    grillas

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -3,50 +3,81 @@ export default function Body() {
   return (
     <BodyContainer>
       <div className="div__grilla">
-        <h1>Frontend Stack</h1>
+        <h1 className="h1">Frontend Stack</h1>
         <ul className="grilla">
           <div className="div">
-            <svg></svg>
+            <img src="" alt="" />
             <p className="p">HTML</p>
           </div>
           <div className="div">
-            <svg></svg>
+            <img src="" alt="" />
+
             <p className="p">CSS</p>
           </div>
           <div className="div">
-            <svg></svg>
+            <img src="" alt="" />
+
             <p className="p">React.js</p>
           </div>
           <div className="div">
-            <svg></svg>
+            <img src="" alt="" />
+
             <p className="p">JavaScript</p>
           </div>
           <div className="div">
-            <svg></svg>
+            <img src="" alt="" />
+
             <p className="p">TypeScript</p>
           </div>
           <div className="div">
-            <svg></svg>
+            <img src="" alt="" />
+
             <p className="p">Prettier</p>
           </div>
         </ul>
       </div>
-      <div>
-        <h1>Backend Stack</h1>
+      <div className="div__grilla">
+        <h1 className="h1">Backend Stack</h1>
+        <ul className="grilla">
+          <div className="div">
+            <img src="" alt="" />
+
+            <p className="p">Node.js</p>
+          </div>
+          <div className="div">
+            <img src="" alt="" />
+
+            <p className="p">PostgreSQL</p>
+          </div>
+          <div className="div">
+            <img src="" alt="" />
+
+            <p className="p">Express</p>
+          </div>
+          <div className="div">
+            <img src="" alt="" />
+
+            <p className="p">Sequelize</p>
+          </div>
+        </ul>
       </div>
-      <div>
-        <h1>Proyectos</h1>
+      <div className="div__grilla">
+        <h1 className="h1">Proyectos</h1>
       </div>
     </BodyContainer>
   );
 }
 const BodyContainer = styled.div`
-  border: 1px solid red;
   display: flex;
   flex-flow: column;
+  padding: 10px 10px 10px 10px;
   .div__grilla {
-    border: 1px solid blue;
     width: 1300px;
+    border-radius: 15px;
+    box-shadow: 3px 3px 10px rgba(0, 0, 0, 0.5);
+    background-color: #80808024;
+    padding: 10px 10px 10px 10px;
+    margin-top: 10px;
   }
   .grilla {
     margin: 30px 100px 20px;
@@ -65,8 +96,16 @@ const BodyContainer = styled.div`
   .div {
     width: 100px;
     height: 80px;
+    border: 1px solid white;
+    border-radius: 7px;
+    align-items: center;
+    display: flex;
+    flex-flow: column;
   }
   .p {
+    color: white;
+  }
+  .h1 {
     color: white;
   }
 `;
